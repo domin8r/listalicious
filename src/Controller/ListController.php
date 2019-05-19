50d64668c2307a55fe2874406bcdc7ea5534bea7
Marcel Douwstra <marcel@dgngroep.nl>
2019-05-19 23:40:43 +0200

    setting up entity and api platform

---
+++ src/Controller/ListController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ListController {
+    
+    public function index() {
+        
+    }
+    
+    
+    public function createItem(array $data) {
+        
+    }
+    
+    public function updateItem() {
+        
+    }
+    
+    public function deleteItem() {
+        
+    }
+}
---
+++ src/Entity/ListItem.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ListItemRepository")
+ */
+class ListItem
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_on;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_on;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $deleted_on;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $done;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreatedOn(): ?\DateTimeInterface
+    {
+        return $this->created_on;
+    }
+
+    public function setCreatedOn(\DateTimeInterface $created_on): self
+    {
+        $this->created_on = $created_on;
+
+        return $this;
+    }
+
+    public function getUpdatedOn(): ?\DateTimeInterface
+    {
+        return $this->updated_on;
+    }
+
+    public function setUpdatedOn(?\DateTimeInterface $updated_on): self
+    {
+        $this->updated_on = $updated_on;
+
+        return $this;
+    }
+
+    public function getDeletedOn(): ?\DateTimeInterface
+    {
+        return $this->deleted_on;
+    }
+
+    public function setDeletedOn(?\DateTimeInterface $deleted_on): self
+    {
+        $this->deleted_on = $deleted_on;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDone(): ?bool
+    {
+        return $this->done;
+    }
+
+    public function setDone(bool $done): self
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+}
